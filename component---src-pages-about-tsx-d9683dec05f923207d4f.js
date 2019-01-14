51bda9b096c00361ec8ab06dc1b0cae884e7d73d
Uladzimir Dziomin <uladzimir.dziomin@gmail.com>
2019-01-14 04:40:17 +0300

    Updates

---
+++ component---src-pages-about-tsx-d9683dec05f923207d4f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{151:function(e,t,a){"use strict";a.r(t);var n=a(0),c=a(451),l=a(458),r=a(456),i=a(231),u=a(179);t.default=Object(u.b)(function(){return n.createElement(c.a,null,n.createElement(l.a,{vertical:!0},n.createElement(r.a,{as:"h2"},n.createElement(i.a,{name:"info circle"}),n.createElement(r.a.Content,null,"About"))),n.createElement(l.a,{vertical:!0},n.createElement("p",null,"This is a starter.")))})}}]);
+//# sourceMappingURL=component---src-pages-about-tsx-d9683dec05f923207d4f.js.map
